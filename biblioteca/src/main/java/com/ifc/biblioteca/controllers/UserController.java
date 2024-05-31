2f40999da9bd284575387343561c8fa0b1a6297f
thalesfb <15121152+thalesfb@users.noreply.github.com>
2024-05-30 21:05:49 -0300

    chore: Update jakarta.persistence imports in entity classes

--- biblioteca/src/main/java/com/ifc/biblioteca/controllers/UserController.java
+++ biblioteca/src/main/java/com/ifc/biblioteca/controllers/UserController.java
@@ -2,7 +2,9 @@ package com.ifc.biblioteca.controllers;
 
 import com.ifc.biblioteca.entities.User;
 import com.ifc.biblioteca.services.UserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
--- biblioteca/src/main/java/com/ifc/biblioteca/entities/Administrator.java
+++ biblioteca/src/main/java/com/ifc/biblioteca/entities/Administrator.java
@@ -1,6 +1,6 @@
 package com.ifc.biblioteca.entities;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 
 @Entity
 public class Administrator extends User {
