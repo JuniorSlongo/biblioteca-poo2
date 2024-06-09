c6370e32fd55bfb53c8d671874733ef0c7e75f1f
thalesfb <15121152+thalesfb@users.noreply.github.com>
2024-06-09 18:34:26 -0300

    fix: Update RegisterRequestDTO to include birthDate as Date type

--- biblioteca/src/main/java/com/ifc/library/dto/RegisterRequestDTO.java
+++ biblioteca/src/main/java/com/ifc/library/dto/RegisterRequestDTO.java
@@ -1,3 +1,5 @@
 package com.ifc.library.dto;
 
-public record RegisterRequestDTO (String name, String email, String password, String cpf, String birthDate, String registration, String departament, String type) {}
+import java.util.Date;
+
+public record RegisterRequestDTO (String name, String email, String password, String cpf, Date birthDate, String registration, String departament, String type) {}
